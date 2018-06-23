a36888627be9cde472ec83ebf474cc950141c269
Pacs Yuu <jpaclardev@gmail.com>
2018-06-23 19:46:32 +1200

    First commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    const string TAG_PLAYER = "Player";
+    GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag(TAG_PLAYER);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = _player.transform.position;
+	}
+}
